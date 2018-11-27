9e06d5a106e1b7973231a99a805bfcfd98fef6b2
savi30 <savianubogdan@gmail.com>
2018-11-27 21:23:20 +0200

    env vars

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: 'AIzaSyAzF1UVl8Dx-eIc_HYAoodwfyJ8LTv5Y94',
+  authDomain: 'appointment-f6c22.firebaseapp.com',
+  databaseURL: 'https://appointment-f6c22.firebaseio.com',
+  projectId: 'appointment-f6c22',
+  storageBucket: 'appointment-f6c22.appspot.com',
+  messagingSenderId: '599515404757'
+};
+

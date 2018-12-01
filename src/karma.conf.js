8cbdb7adfc09a034893fd1ee3f0c64af717f593b
savi30 <savianubogdan@gmail.com>
2018-12-01 02:01:54 +0200

    lint & tests

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -3,6 +3,13 @@
 
 module.exports = function (config) {
   config.set({
+    browsers: ['CustomHeadlessChrome'],
+    customLaunchers:{
+      CustomHeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: ['--disable-translate', '--disable-extensions', '--remote-debugging-port=9223', '--no-sandbox']
+      }
+    },
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
